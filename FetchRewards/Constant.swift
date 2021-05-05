d8162b1d07c3c17defdc86b56f7e681ed3222816
Changhyun Kim <changhyun.kim@sjsu.edu>
2021-05-05 00:33:48 -0700

    Minor change

--- FetchRewards/Constant.swift
+++ FetchRewards/Constant.swift
@@ -7,9 +7,6 @@
 
 import Foundation
 
-/**
- Save client id and app secret keys which is for API communication
- */
-
+// Save client id and app secret keys which is for API communication
 let clientId = "MjE3NzA0MTR8MTYxOTEzMjg5NS43Njc1NjA1"
 let appSecret = "e8ea6839cfc79ee344cab25be29ed2fe10586f46ce66157aa6c4ef6f3e8082dd"
